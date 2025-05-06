08be8b610f5a9d16190cb5205dd7f38c95d4877b
guyue <14955762+guyue8@user.noreply.gitee.com>
2025-05-06 23:39:02 +0800

    去掉compose改为传统的xml布局；
     解决 java.lang.IllegalStateException: You need to use a Theme.AppCompat theme (or descendant) with this activity. 的核心就是修改您的自定义主题 Theme.JustBuy，使其父主题继承自 Theme.AppCompat 主题家族中的一个。

--- app/src/main/java/com/example/justbuy/MainActivity.kt
+++ app/src/main/java/com/example/justbuy/MainActivity.kt
@@ -1,47 +1,17 @@
 package com.example.justbuy
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.justbuy.ui.theme.JustBuyTheme
+import androidx.appcompat.app.AppCompatActivity // 导入 AppCompatActivity 作为 Activity 的基类
 
-class MainActivity : ComponentActivity() {
+// MainActivity 现在继承自 AppCompatActivity，这是传统的 View 系统中常用的 Activity 基类
+class MainActivity : AppCompatActivity() {
+
+    // onCreate 是 Activity 的生命周期方法，在 Activity 创建时调用
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            JustBuyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        // 设置 Activity 的内容视图为之前创建的 XML 布局文件
+        setContentView(R.layout.activity_main)
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JustBuyTheme {
-        Greeting("Android")
-    }
-}
